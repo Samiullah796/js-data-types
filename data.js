6192c75d9603e76b81519e2eb4d804aded9bd6d4
Samiullah796 <samikhan2021karachi@gmail.com>
2024-08-30 17:27:00 +0500

    js data types

---
+++ data.js
@@ -0,0 +1,16 @@
+console.log("Hey I hope u r fine")
+
+let a = 5;
+a = a + 1
+let b = 6;
+let c = "Sami";
+let _a = "Pakistan";
+// vae 55a = "india"; not allowed
+// console.log(a + b + 8)
+// console.log(typeof a, typeof b, typeof c)
+{
+    console.log(a)
+}
+
+const a1 = 6;
+// a1= a1 + 1; not allowed because a1 is constant
